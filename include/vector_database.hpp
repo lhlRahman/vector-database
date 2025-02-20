594cfd3efb0ab2914bb6185532e047d4c4bb99d6
Abdul <lhlrahman@gmail.com>
2025-02-20 00:33:23 -0500

    feat: add metadata support to VectorDatabase header

--- include/vector_database.hpp
+++ include/vector_database.hpp
@@ -1,4 +1,5 @@
 // include/vector_database.hpp
+
 #pragma once
 #include "../src/core/vector.hpp"
 #include "../src/core/kd_tree.hpp"
@@ -17,17 +18,27 @@ private:
     size_t dimensions;
     bool useApproximate;
     std::unordered_map<std::string, Vector> vectorMap;
+    std::unordered_map<std::string, std::string> metadataMap;
 
 public:
+    struct SearchResult {
+        std::string key;
+        float distance;
+        std::string metadata;
+    };
+
     VectorDatabase(size_t dimensions, bool useApproximate = false, size_t numTables = 10, size_t numHashFunctions = 8);
     void setDistanceMetric(std::shared_ptr<DistanceMetric> metric);
     void insert(const Vector& vector, const std::string& key);
+    void insert(const Vector& vector, const std::string& key, const std::string& metadata);
     void batchInsert(const std::vector<Vector>& vectors, const std::vector<std::string>& keys);
     std::vector<std::pair<std::string, float>> similaritySearch(const Vector& query, size_t k) const;
+    std::vector<SearchResult> similaritySearchWithMetadata(const Vector& query, size_t k) const;
     std::vector<std::vector<std::pair<std::string, float>>> batchSimilaritySearch(const std::vector<Vector>& queries, size_t k) const;
     void toggleApproximateSearch(bool useApproximate);
     void loadFromFile(const std::string& filename);
     void saveToFile(const std::string& filename) const;
+    std::string getMetadata(const std::string& key) const;
     size_t getDimensions() const { return dimensions; }
     bool isUsingApproximateSearch() const { return useApproximate; }
     const std::unordered_map<std::string, Vector>& getAllVectors() const { return vectorMap; }
